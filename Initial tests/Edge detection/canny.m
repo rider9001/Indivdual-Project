302fa15a51aab80231250a1ad152606e3e0e0f40
Edward <morgandavies428@gmail.com>
2022-10-09 20:15:45 +0100

    tested sobel filter method for image processing in MATLAB

--- Initial tests/Edge detection/canny.m
+++ Initial tests/Edge detection/canny.m
@@ -1,5 +1,6 @@
-I = imread('coins.png');
-imshow(I);
+I = imread('img2.jpg');
+I = flip(flip(I, 1), 2);
+I = im2gray(I);
 
 BW1 = edge(I,'sobel');
 BW2 = edge(I,'canny');
@@ -8,10 +9,12 @@ tiledlayout(1,3);
 
 nexttile
 imshow(BW1)
+imwrite(BW1, 'sobel.png');
 title('Sobel Filter');
 
 nexttile
 imshow(BW2);
+imwrite(BW2, 'canny.png');
 title('Canny Filter');
 
 nexttile
--- Initial tests/Edge detection/filter_testing.m
+++ Initial tests/Edge detection/filter_testing.m
@@ -1,6 +1,7 @@
 clear;
-I = imread('coins.png');
-
-I_edge = sobel_filter(I);
+I = imread('testCleanPlate.jpg');
+%I = flip(flip(I, 1), 2);
+I_edge = sobel_filter(I, 190);
 
 imshow(I_edge);
+imwrite(I_edge, 'out.jpg');
