3d9fff4c8d481df1e26071d97181b7cb184b9bd8
Edward <morgandavies428@gmail.com>
2022-10-17 00:33:11 +0100

    tried a few new plate detection techniques, also created a few new test shots

--- Initial tests/Edge detection/filter_testing.m
+++ Initial tests/Edge detection/filter_testing.m
@@ -1,13 +1,18 @@
 clear;
-I = imread('testCleanPlate.jpg');
+I = imread('plateFar/plateFar1080p.png');
 %I = flip(flip(I, 1), 2);
-I_edge = robert_filter(I, 120);
+I_edge = imbinarize(sobel_filter(I, 210));
 
-imshow(I_edge);
+I_edge = bwareafilt(I_edge, [100, 1000]);
+
+% find both black and white regions
+% stats = [regionprops(I_edge); regionprops(not(I_edge))];
 % hold on;
 % for i = 1:numel(stats)
 %     rectangle('Position', stats(i).BoundingBox, ...
 %     'Linewidth', 1, 'EdgeColor', 'r', 'LineStyle', '--');
 % end
+
+imshow(I_edge);
 
 imwrite(I_edge, 'out.jpg');
