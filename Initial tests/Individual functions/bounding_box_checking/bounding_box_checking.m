910684f70f63897d75e32cf3b294a38787b43b9b
Edward <morgandavies428@gmail.com>
2022-10-25 01:20:05 +0100

    tried bounding box filtering techiques, need to work on image motion bounding

--- Initial tests/Individual functions/bounding_box_checking/bounding_box_checking.m
+++ Initial tests/Individual functions/bounding_box_checking/bounding_box_checking.m
@@ -1,8 +1,8 @@
 clear;
 inpImg = double(im2gray(imread('img2.jpg')));
 
-kernel = 19;
-I_out_blur = gauss_blur(inpImg, kernel);
+kernelSize = 19;
+I_out_blur = gauss_blur(inpImg, kernelSize);
 
 thresh = 150;
 %I_out_edge = sobel_filter(I_out_blur, thresh);
@@ -10,25 +10,80 @@ I_out_edge = edge(I_out_blur, 'sobel');
 
 imshow(I_out_edge);
 %find both black and white regions
-stats = [regionprops(I_out_edge); regionprops(not(I_out_edge))];
+boundedAreas = [regionprops(I_out_edge); regionprops(not(I_out_edge))];
 
 %help = stats(j).BoundingBox;
 %bounding box index: [origin x, origin y, width, height]
 
+passList = [];
+
 ratio = 1.58; % 79mm / 50mm
 tolerance = 0.2; %percentage value 0-1
 
-validBoxes = [];
+%bounding box size limits percent of image width
+lowBoundLim = 0.01;
+highBoundLim = 0.04;
 
-hold on;
-for j = 1:numel(stats)
+for i = 1:numel(boundedAreas)
+    box = boundedAreas(i).BoundingBox;
 
-    box = stats(j).BoundingBox;
     boxRatio = box(4) / box(3);
+    %find box ratio and compare to ratio for standard licence plate letter
     if (boxRatio < (ratio + ratio * tolerance)) && (boxRatio > (ratio - ratio * tolerance))
-       validBoxes = [validBoxes stats(j)];
-       rectangle('Position', stats(j).BoundingBox, ...
-       'LineWidth', 1, 'EdgeColor', 'g', 'LineStyle', '--');
+        passList = [passList, boundedAreas(i)];
+    end
+end
+
+%red areas pass the ratio filter
+for i = 1:numel(passList)
+       rectangle('Position', passList(i).BoundingBox, ...
+        'LineWidth', 1, 'EdgeColor', 'r', 'LineStyle', '--');
+end
+
+boundedAreas = passList;
+passList = [];
+
+imWidth = size(I_out_edge, 2);
+
+%crude width filtering
+for i = 1:numel(boundedAreas)
+    box = boundedAreas(i).BoundingBox;
+
+    if (box(3) > imWidth * lowBoundLim) && (box(3) < imWidth * highBoundLim)
+        passList = [passList, boundedAreas(i)];
+    end
+end
+
+%yellow areas pass the size filter
+for i = 1:numel(passList)
+       rectangle('Position', passList(i).BoundingBox, ...
+        'LineWidth', 1, 'EdgeColor', 'y', 'LineStyle', '--');
+end
+
+boundedAreas = passList;
+passList = [];
+
+%round to nearest 100 and take mode intersection of x and y
+xCoordList = [];
+yCoordList = [];
+
+for i = 1:numel(boundedAreas)
+    xCoordList = [xCoordList, boundedAreas(i).BoundingBox(1)];
+    yCoordList = [yCoordList, boundedAreas(i).BoundingBox(2)];
+end
+
+%round each list to nearest 100 for y, 1000 for x
+xCoordList = round(xCoordList, -3);
+yCoordList = round(yCoordList, -3);
+xCoordMode = mode(xCoordList);
+yCoordMode = mode(yCoordList);
+
+for i = 1:numel(boundedAreas)
+    box = boundedAreas(i).BoundingBox;
+    %if bounding box has x,y origin of the mode of all bounding boxes
+    %rounded to 100
+    if (round(box(1), -3) == xCoordMode) && (round(box(2), -3) == yCoordMode)
+        passList = [passList, boundedAreas(i)];
     end
 end
 
@@ -37,19 +92,26 @@ y1 = 0;
 wid = 0;
 high = 0;
 
-for i = 1:numel(validBoxes)
-    x1 = x1 + validBoxes(i).BoundingBox(1);
-    y1 = y1 + validBoxes(i).BoundingBox(2);
-    wid = wid + validBoxes(i).BoundingBox(3);
-    high = high + validBoxes(i).BoundingBox(4);
+for i = 1:numel(passList)
+    x1 = x1 + passList(i).BoundingBox(1);
+    y1 = y1 + passList(i).BoundingBox(2);
+    wid = wid + passList(i).BoundingBox(3);
+    high = high + passList(i).BoundingBox(4);
 end
 
 
-x1 = x1 / numel(validBoxes);
-wid = wid / numel(validBoxes);
-y1 = y1 / numel(validBoxes);
-high = high / numel(validBoxes);
+x1 = x1 / numel(passList);
+wid = wid / numel(passList);
+y1 = y1 / numel(passList);
+high = high / numel(passList);
 
+%green pixels are assumed to be the licence plate characters
+for i = 1:numel(passList)
+       rectangle('Position', passList(i).BoundingBox, ...
+        'LineWidth', 1, 'EdgeColor', 'g', 'LineStyle', '--');
+end
+
+%blue is the average of the green boxes
 rectangle('Position', [x1, y1, wid, high], ...
 'LineWidth', 1, 'EdgeColor', 'b', 'LineStyle', '--');
 
