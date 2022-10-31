21798d131febd8930b624def7a410fb6440c9831
Edward <morgandavies428@gmail.com>
2022-10-31 21:35:15 +0000

    created working motion detector, need to think about trying the non-maximum supression

--- Initial tests/Individual functions/motion_bounding/motion_bounding.m
+++ Initial tests/Individual functions/motion_bounding/motion_bounding.m
@@ -3,7 +3,7 @@ delete('out/frame/*');
 delete('out/base/*');
 delete('out/mask/*');
 
-vid = VideoReader('plateHeadOnMotion1080p.mp4');
+vid = VideoReader('walkingPastCam720p.mp4');
 ptr = read(vid);
 frameCount = vid.NumFrames;
 
@@ -12,16 +12,16 @@ backGrndFrameNo = 1;
 
 motionThreshold = 50;
 
-backGrndFrame = gauss_blur(im2gray(ptr(:, :, :, backGrndFrameNo)), kernelSize);
+backGrndFrame = sobel_filter(gauss_blur(im2gray(ptr(:, :, :, backGrndFrameNo)), kernelSize));
 imwrite(backGrndFrame, 'out/frame/base.png');
 
-for frame = 1:10:frameCount
+for frame = 1:5:frameCount
     if( frame == backGrndFrameNo )
         continue;
     end
 
     fileNm = strcat(num2str(frame), '.png');
-    frameB = gauss_blur(im2gray(ptr(:, :, :, frame)), kernelSize);
+    frameB = sobel_filter(gauss_blur(im2gray(ptr(:, :, :, frame)), kernelSize));
     imwrite(frameB, sprintf('out/base/%s', fileNm));
 
     frameMot = imsubtract(frameB, backGrndFrame);
@@ -54,26 +54,29 @@ for frame = 1:10:frameCount
             end
         end
     end
-    fprintf('Bounding box: (%i,%i) to (%i,%i)\n', lowestX, lowestY, highestX, highestY);
+    %fprintf('Bounding box: (%i,%i) to (%i,%i)\n', lowestX, lowestY, highestX, highestY);
 
     %if highest x/y remain out of bounds, then no pixels above the
     %threshold exist, no motion detected, can toss image
 
     %mask image outside of motion range (mask whole image for no motion)
     %j = y axis, i = x axis
-    if(lowestX > size(frameB, 2))
-        out = zeros(size(frameB));
-        imwrite(out, sprintf('out/mask/%s', fileNm));
+    if( (lowestX > size(frameB, 2)) || (highestX - lowestX < 100) || (highestY - lowestY < 100) )
+        %if no motion detected or motion area is too small (assume noise if this is the case)
+        imwrite(ones(size(frameB)), sprintf('out/mask/%s', fileNm));
+        fprintf(sprintf('frame %i: none or insignificant motion detected.\n', frame));
     else
         for i = 1:size(frameB, 2)
             for j = 1:size(frameB, 1)
-                if( (j > highestY || j > lowestY) || (i > highestX || i < lowestX) )
+                if( ~((j > lowestY && j < highestY) && (i > lowestX && i < highestX)) )
+                    %fprintf('drawing at: (%i,%i)\n', i, j);
                     frameB(j,i) = 0;
                 end
             end
         end
 
         imwrite(frameB, sprintf('out/mask/%s', fileNm));
+        fprintf(sprintf('frame %i: motion detected.\n', frame));
     end
 
     fprintf('%i out of %i completed\n', frame, frameCount);
